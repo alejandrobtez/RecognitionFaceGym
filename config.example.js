ff1dc540940008e964df7effc3de7b61acb86686
Alejandro <alejandrobenitez91203@gmail.com>
2026-02-09 09:57:38 +0100

    Delete config.example.js

--- config.example.js
+++
@@ -1,4 +0,0 @@
-// config.example.js
-// IMPORTANTE: Renombra este archivo a 'config.js' y pon tus propias URLs de Azure
-const URL_ACCESO = "PEGA_AQUI_TU_LOGIC_APP_URL_ACCESO";
-const URL_INFO   = "PEGA_AQUI_TU_LOGIC_APP_URL_INFO";
